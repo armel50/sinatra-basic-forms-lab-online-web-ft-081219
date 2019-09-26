2eb6c1d7015976616df84a018cf9e1e233eeb481
Cloe Kouadjo <armelfli50@gmail.com>
2019-09-26 14:35:02 +0000

    Automatically backed up by Learn

--- models/puppy.rb
+++ models/puppy.rb
@@ -1,7 +1,7 @@
 class Puppy 
   attr_accessor :name, :months_old
   attr_reader :breed 
-  def initialize(name:, breed:, months_old:) 
+  def initialize(name, breed, months_old) 
     @name = name 
     @breed = breed
     @months_old = months_old
